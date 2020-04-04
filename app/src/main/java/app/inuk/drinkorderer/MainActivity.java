9d3963daf6e58951b6cc99bdc41acc658f48e35a
Andreas Work <awork@ruc.dk>
2020-04-04 12:36:25 +0200

    init more options

--- app/src/main/java/app/inuk/drinkorderer/MainActivity.java
+++ app/src/main/java/app/inuk/drinkorderer/MainActivity.java
@@ -1,14 +1,34 @@
 package app.inuk.drinkorderer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button buttonMore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonMore = findViewById(R.id.buttonMore);
+
+        buttonMore.setOnClickListener(this);
+
+    }
+
+    public void onClick(View view) {
+        if (view == buttonMore) {
+            Intent moreOptions = new Intent(this, MoreOptions.class);
+            startActivity(moreOptions);
+            System.out.println("Knap 1");
+        }
     }
 }
